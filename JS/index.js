4e0ebc4bcf917972ebe39571a586aa01b6ba2088
Andhika Setiawan <55912245+andikasetiawan@users.noreply.github.com>
2024-05-23 23:31:24 +0800

    update 5

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,5 @@
 const aInput = document.getElementById('Alas');
 const bInput = document.getElementById('Tinggi');
-const hInput = document.getElementById('Luas');
 const calculateButton = document.getElementById('calculate');
 const resultElement = document.getElementById('result');
 
@@ -9,11 +8,10 @@ calculateButton.addEventListener('click', calculateTrapezoid);
 function calculateTrapezoid() {
   const a = parseFloat(aInput.value);
   const b = parseFloat(bInput.value);
-  const h = parseFloat(hInput.value);
-  const area = calculateTrapezoidArea(Alas, Tinggi, Luas);
+  const area = calculateTrapezoidArea(Alas, Tinggi);
   resultElement.textContent = `The area of the trapezoid is: ${area}`;
 }
 
-function calculateTrapezoidArea(Alas, Tinggi, Luas) {
-  return (1/2) * (Alas + Tinggi) * Luas;
+function calculateTrapezoidArea(Alas, Tinggi) {
+  return (1/2) * (Alas + Tinggi);
 }
